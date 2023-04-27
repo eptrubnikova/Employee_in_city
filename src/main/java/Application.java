48e6e064c24a57e0813f993a6296aca55c48ba85
eptrubnikova <eptrubnikova@gmail.com>
2023-04-27 22:30:21 +0600

    перевод на hibernate

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,4 @@
-import model.City;
 import model.Employee;
-import service.EmployeeDAO;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,61 +8,22 @@ public class Application {
 
     public static void main(String[] args) throws SQLException {
 
-        final String user = "postgres";
-        final String password = "14042017Katya!";
-        final String url = "jdbc:postgresql://localhost:5432/employee";
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
 
-        try (final Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee WHERE id = (?)")) {
+        Employee employee = new Employee("Eva", "Ivanova", "f", 30, 6);
+        employeeDAO.create(employee);
 
-            // Подставляем значение вместо wildcard
-            statement.setInt(1, 4);
+        System.out.println(employeeDAO.readById(3));
 
-            // Делаем запрос к базе и результат кладем в ResultSet
-            final ResultSet resultSet = statement.executeQuery();
+        List<Employee> list = employeeDAO.readAllEmployee();
 
-            // Методом next проверяем есть ли следующий элемент в resultSet
-            // и одновременно переходим к нему, если таковой есть
-            while (resultSet.next()) {
-
-                // С помощью методов getInt и getString получаем данные из resultSet
-                String nameOfEmployee = "Name: " + resultSet.getString("first_name");
-                String lastNameOfEmployee = "Last_name: " + resultSet.getString("last_name");
-                String genderOfEmployee = "Last_name: " + resultSet.getString("gender");
-                String cityOfEmployee = "City_id: " + resultSet.getInt("city_id");
-
-                // Выводим данные в консоль
-                System.out.println(nameOfEmployee);
-                System.out.println(lastNameOfEmployee);
-                System.out.println(genderOfEmployee);
-                System.out.println(cityOfEmployee);
-
-            }
+        for (Employee employees : list) {
+            System.out.println(employees);
         }
 
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+        Employee employee1 = new Employee("Vera", "Penkina", "f", 35, 5);
+        employeeDAO.updateById(employee1);
 
-            // Создаем объект класса BookDAOImpl
-            EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
-
-            Employee employee = new Employee( "Eva", "Ivanova", "f", 30, new City(6));
-
-            // Вызываем метод добавления объекта
-            employeeDAO.create(employee);
-
-            // Создаем список наполняя его объектами, которые получаем
-            // путем вызова метода для получения всех элементов таблицы
-            List<Employee> list = new ArrayList<>(employeeDAO.readAllEmployee());
-
-            employeeDAO.updateById(1, "Evunov");
-
-            employeeDAO.deleteById(13);
-
-            // Выведем список в консоль
-            for (Employee employees : list) {
-                System.out.println(employees);
-            }
-
-        }
+        employeeDAO.delete(employee);
     }
 }
